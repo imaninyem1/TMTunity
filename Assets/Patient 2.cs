a4b3fcbf1d77dad6ead16a18e4db4c3e7c6e5ec3
Imani Warren <iwarren@mail.sfsu.edu>
2020-03-06 11:05:14 -0800

    Added blood script

---
+++ Assets/Patient 2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+class BloodModerator : MonoBehaviour
+{
+    public Patient patient;
+    public ObiEmitter emitter;
+
+    void FixedUpdate()
+    {
+    // if statement to trigger Imani's bleeding visuals
+        string bloodStatus = patient.bloodStatus;
+
+        switch (bloodStatus)
+        {
+            case "Heaviest Bleeding":
+                emitter.NumParticles = 3000;
+                break;
+            case "Heavy Bleeding":
+                emitter.NumParticles = 2000;
+                break;
+            case "Moderate Bleeding":
+                emitter.NumParticles = 1500;
+                break;
+            case "Low Bleeding":
+                emitter.NumParticles = 650;
+                break;
+            case "Lowest Bleeding":
+                emitter.NumParticles = 250;
+                break;
+            case "No Bleeding":
+                emitter.NumParticles = 50;
+                break;
+        }
+    }
+
+}
