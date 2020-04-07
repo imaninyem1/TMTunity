a5cbc47355464963ee6511481e429f60b88ee1f9
Adrian <achoppa@mail.sfsu.edu>
2020-04-06 17:38:05 -0700

    pressure location grid

---
+++ Assets/SphereControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour
+{
+    public KeyCode key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key)) 
+        {
+            print("hello");
+            gameObject.SetActive(true);
+        }
+
+        else if (Input.GetKeyUp(key))
+        {
+           gameObject.SetActive(false);
+        }
+        
+    }
+}
