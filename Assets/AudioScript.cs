170bb7dc5f7a54cbe7592bd2a025e85e05524624
Kaila_Marie <kgemenes@mail.sfsu.edu>
2020-04-13 16:40:12 -0700

    AudioScript for random clip to play

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioScript : MonoBehaviour {
+
+    public Patient patient;
+    public AudioClip[] clips; //array 
+    public AudioMixerGroup output;
+    public string prevStatus;
+
+    // Update is called once per frame
+    void Update() {
+
+
+        string healthStatus = patient.healthStatus;
+
+
+           if (healthStatus != prevStatus) {
+               switch (healthStatus) {
+                    case "Stable Wounded":
+                        PlaySound();
+                        break;
+                    case "Moderately Wounded":
+                        PlaySound();
+                        break;
+                    case "Severly Wounded":
+                        PlaySound();
+                        break;
+                    case "Deadly Wounded":
+                        PlaySound();
+                        break;
+                    case "Dead":
+                        PlaySound();
+                        break;
+                }
+           }
+           prevStatus = healthStatus; 
+    
+
+        if (Input.GetKeyDown("space")) {
+            PlaySound(); 
+        }
+    }
+
+       void PlaySound() {
+            //Randomize 
+            int randomClip = Random.Range (0,clips.Length);
+
+            //Create an AudioSource component on object
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+
+            //Load clip into the AudioSource to play sound
+            source.clip = clips[randomClip];
+
+            //Set the output for AudioSource
+            source.outputAudioMixerGroup = output;
+
+            //Play the clip
+            source.Play(0);
+            //source.PlayOneShot(); 
+
+            //Destroy the AudioSource after length of audio clip is done playing
+            Destroy(source, clips[randomClip].length);
+        }
+
+}
--- Assets/SoldierAnimations.cs
+++ Assets/SoldierAnimations.cs
@@ -6,11 +6,7 @@ public class SoldierAnimations : MonoBehaviour {
 
     public Animator animator;
     public Patient patient; //reference to Patient script in scene
-    public AudioSource stableWounded;
-    public AudioSource moderatelyWounded;
-    public AudioSource severelyWounded;
-    public AudioSource deadlyWounded;
-    public AudioSource dead;
+
 
     // Update is called once per frame
     void Update() {
@@ -22,33 +18,18 @@ public class SoldierAnimations : MonoBehaviour {
         float health = patient.health;
         if (health <= 100 && health > 75) {
             animator.SetInteger("painStatus", 0);
-            if (stableWounded.time == 0) {  
-                stableWounded.Play(0); //play audio
-            }
         }
         else if (health <= 75 && health > 50) {
             animator.SetInteger("painStatus", 1);
-            if (moderatelyWounded.time == 0) {
-                moderatelyWounded.Play(0);
-            }
         }
         else if (health <= 50 && health > 25) {
             animator.SetInteger("painStatus", 2);
-            if (severelyWounded.time == 0) {
-                severelyWounded.Play(0);
-            }
         }
         else if (health <= 25 && health > 0) {
             animator.SetInteger("painStatus", 3); 
-            if (deadlyWounded.time == 0) {
-                deadlyWounded.Play(0);
-            }
         }
         else {
             animator.SetInteger("painStatus", 4);
-            if (dead.time == 0) {
-                dead.Play(0);
-            }
         }
         
 
